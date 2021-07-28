8135f2c42f495716d0b5945475e22493bb4740c7
kazi-rayed-hossain <khossain162030@bscse.uiu.ac.bd>
2021-07-28 10:38:10 +0600

    ListView Builder, List Generate, Card & Asserts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:dhur_hoy_na_ken/models/catalog.dart';
 import 'package:dhur_hoy_na_ken/widgets/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:dhur_hoy_na_ken/widgets/item_widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,23 +14,28 @@ class HomePage extends StatelessWidget {
   @override
 
   //BuildContext context holo kon jinish er location koi ta bole
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(20,(index)=>CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-        
-      body : Center(
-        child: Container(
-          child: Text('hutu tutu $days $name'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidgets(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: const myDrawer(),
     );
   }
 
-
   // bringvegetables(){
-    
+
   // }
 }
